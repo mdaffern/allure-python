3999c79dfed056e986cc7ebd2b6a3b95207f1c02
Mike <michael.dafferner@gmail.com>
2019-08-13 13:14:38 -0400

    Create test_test.py

---
+++ test_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.allure.step
+def make_test_data_foo():
+    pass
+
+def test_foo():
+    assert make_some_data_foo() is not None
+
+@pytest.allure.step('make_some_data_foo')
+def make_some_data_bar():
+    pass
+
+def test_bar():
+    assert make_some_data_bar() is not None
